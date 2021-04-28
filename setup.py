597ce5ee89ab076bfc8094489924ad71b0580e29
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2021-04-27 23:28:02 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='auxiliarygoose',
+    version='0.1.0',
+    packages=['auxiliarygoose',],
+    license='Apache 2.0',
+    long_description="Additional story providers for Goosepaper",
+)
